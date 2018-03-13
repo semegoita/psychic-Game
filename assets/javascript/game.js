05cb50831691f924b084e2c4b0a7a54f378a87d8
semegoita <34927635+semegoita@users.noreply.github.com>
2018-03-12 17:05:05 -0700

    Variables declared

---
+++ assets/javascript/game.js
@@ -0,0 +1,10 @@
+var won = document.getElementById("wins");
+var lost = document.getElementById("losses");
+var gleft = document.getElementById("left");
+var soMade = document.getElementById("so-far");
+
+var letters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+var won = 0;
+var lost = 0;
+var gleft =9;
+var soMade =0;
