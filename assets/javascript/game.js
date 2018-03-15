07143ef24cff81aa41073673e914d2e4e5a8610d
semegoita <34927635+semegoita@users.noreply.github.com>
2018-03-14 23:09:32 -0700

    all functions and methods added

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -13,7 +13,48 @@ var winnerGuess;
 resetGame();
 display();
 
-
-
-
-
+document.onkeyup = function(event) {
+    var guess = event.key;
+    if (guess === letterToGuess) {
+      win();
+    } else if (guessesLeft - 1 === 0) {
+      lost();
+    } else {
+      fail(guess);
+    }
+  
+    display();
+  }
+  
+  function display() {
+    var winsP = document.getElementById("wins");
+    var losesP = document.getElementById("loses");
+    var guessLeft = document.getElementById("guessLeft");
+    var letterGuessed = document.getElementById("guessed");
+    winsP.innerHTML = wins;
+    losesP.innerHTML = loses;
+    guessLeft.innerHTML = guessesLeft;
+    letterGuessed.innerHTML = guessedLetters.join(',');
+  }
+  
+  function win() {
+    wins++;
+    resetGame();
+  }
+  
+  function lost() {
+    loses++;
+    resetGame();
+  }
+  
+  function fail(letter) {
+    guessesLeft--;
+    guessedLetters.push(letter);
+  }
+  
+  function resetGame() {
+    guessesLeft = 9;
+    guessedLetters = [];
+    letterToGuess = letters[Math.floor(Math.random() * letters.length)];
+    console.log("Letter to guess: " + letterToGuess);
+  }
