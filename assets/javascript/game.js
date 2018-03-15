d8d8fd7e52252a812619533d365eeda9b7693122
semegoita <34927635+semegoita@users.noreply.github.com>
2018-03-14 22:17:57 -0700

    variable names modified to fit description

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,14 +1,19 @@
-var won = document.getElementById("wins");
-var lost = document.getElementById("losses");
-var gleft = document.getElementById("left");
-var soMade = document.getElementById("so-far");
+// var won = document.getElementById("wins");
+// var lost = document.getElementById("losses");
+// var gleft = document.getElementById("left");
+// var soMade = document.getElementById("so-far");
 
 var letters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
-var won = 0;
-var lost = 0;
-var gleft =9;
-var guessesMade =[];
+var wins = 0;
+var loses = 0;
+var guessedLetters;
+var guessesLeft;
+var winnerGuess;
 
-document.onkeyup = function(event){
+resetGame();
+display();
 
-}
+
+
+
+
